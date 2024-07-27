8b65a8a4491d417c04aa164aac95ddca46861624
root <root@skeita87.lan>
2024-07-26 21:03:46 -0400

    Install package

---
+++ install_any_linux_package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Autor: keita
+#Discription: This is for install the httpd package
+#Date creation: 07/25/2024
+#Date of mofity: 07/26/2024
+
+
+echo "Pleass enter the package for install in server"
+read package_name
+
+sudo yum install $package_name -y
+sudo systemctl enable $package_name
+sudo systemctl restart $package_name
+
+
+
+
